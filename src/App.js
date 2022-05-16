12e37ddda89dc17ae0ce21869a75c0f20d484b67
Eman Alsayed <emanalsayed210@gmail.com>
2022-05-16 15:02:30 +0300

    design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React,{Component} from 'react';
+
 import './App.css';
 import Navbar from './component/Navbar';
 import {BrowserRouter, Route,Routes} from "react-router-dom";
@@ -8,6 +8,8 @@ import Game from './pages/Game';
 import About_Us from './pages/About_Us';
 import ConnectWithUs from './pages/ConnectWithUs';
 import Log_Out from './pages/Log_Out';
+import React, { useEffect,useState } from 'react'
+import axios from 'axios'
 
 
 
@@ -17,6 +19,17 @@ import Log_Out from './pages/Log_Out';
 // import Game from './pages/Game';
 // import ConnectWithUs from './pages/ConnectWithUs';
 function App() {
+
+  const [getData, setgetData]=useState([])
+
+  useEffect(() =>{
+    axios.get("https://api.github.com/users/ariv797/repos")
+    .then(showData =>{
+      console.log(showData)
+      setgetData(showData.data)
+    })
+  
+  })
   return (
     <>
    
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,146 @@
-import React, { Component } from 'react'
-//import axios from 'axios'
 
-class Home extends Component{
+import './Home.css'
 
- 
- 
-
-  // componentDidMount(){
-  //   axios.get("https://api.github.com/users/ariv797/repos")
-  //   .then(res => {
-  //     console.log(res)
-  //   })
- 
- // }  
-  render () {
-
-    return(
-      <div className='home'>
-        <h1>Eman Alsayed</h1>
-      </div>
-    )
-  }
+ const Home = () => {
+  return (
+   
+      <table>
+      <thead>
+        <tr>
+          <th>Project Name</th>
+          <th>Language</th>
+          <th>Discription</th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+      </table>
+   
+  )
 }
 
 export default Home
+
+
+
+
+
+
+// function Home() {
+
+
+
+//   return (
+// <div>
+      
+
+
+//  <table className='table'>
+         
+//   <tr>
+//     <th>Project Name</th>
+//     <th>Language</th>
+//     <th className='disc'>Discription</th>
+//   </tr>
+ 
+//   <tr>
+//     <td>" " </td>
+//     <td>" "</td>
+//     <td>" "</td>
+//   </tr>
+ 
+
+//   </table>
+  {/* <ul>
+  <div className='project'>
+  { getData.map(dataItem => (<li key={dataItem.id}>
+  <tr>
+    <th>{dataItem.name}</th>
+    <th>{dataItem.language || "No Language"}</th>
+    <th>{dataItem.description || "No"}</th>
+  </tr>
+  </li> ))}
+  </div>
+  </ul>
+</table> */}
+  
+         
+ 
+ //</div>
+
+      
+
+
+          // {/* <ul>
+    //   { getData.map(dataItem => (<li /*key={dataItem.id}*/>
+       //  {dataItem.full_name} </li>))}
+    //  </ul>
+       //   </div>
+         // </li>
+       //   ))}
+
+   
+
+    //  {/* <ul>
+      // { getData.map(dataItem => (<li key={dataItem.id}>
+       //   {dataItem.language} </li>))}
+    //  </ul> */}
+
+
+
+   
+//  )
+ // )}
+  
+//export default Home
+
+
+// const baseURL ="https://api.github.com/users/ariv797/repos";
+
+// function Home (){
+  
+//   const [getData,setgetData]= useState(false)
+//   React.useEffect(()=>{
+//     axios.get(baseURL)
+//     .then(getDataFromURL =>{
+//       setgetData(getDataFromURL.data)
+
+//     })
+//   })
+//   return(
+//    <div className='data'>
+//      {this.useState.getData.map(getData => <div>{getData.name}</div>)}
+//    </div>  
+//   )
+// }
+
+// class Home extends Component{
+
+//  state={
+//    user:[]
+//  }
+ 
+
+//   componentDidMount(){
+//     axios.get("https://api.github.com/users/ariv797/repos")
+//     .then(res => { // after I get the data to want to receive on 
+//       console.log(res.data)
+//       this.setState({user:res.data})
+//     })
+ 
+//  }  
+//   render () {
+
+//     return(
+//       <div className='home'>
+//         <h1>Eman Alsayed</h1>
+     
+//       <div className='data'>
+     
+//         {this.state.user.map(users => <div className='eman'>{users.name}</div>)}
+//       </div>
+//       </div>
+//     )
+//   }
+// }
+
+// export default Home
