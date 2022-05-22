1b5f9a9e85198174ae9d85cfbbbb8655c5200cdf
Eman Alsayed <emanalsayed210@gmail.com>
2022-05-22 14:08:40 +0300

    Add table

--- src/pages/TablesWithAPI.js
+++ src/pages/TablesWithAPI.js
@@ -3,25 +3,26 @@ import axios from "axios";
 import {useState,useEffect} from 'react';
 import './TablesWithAPI.css'
 import {gitHubUrl} from './constatn';
+import { gitHubUrl2 } from "./constatn";
 import {TableContainer ,Table , TableHead , TableBody,TableRow ,TableCell ,Paper} from '@mui/material'
 
 
 
 function Tables() {
 
-  // const column =[
-  //   {field:'id' , headerName:'ID' },
-  //   {field:'projectName' , headerName:'Project Name' , width:300 },
-  //   {field:'language' , headerName:'Language',width:300  },
-  //   {field:'description' , headerName:'Description' , width:600 },
-  // ]
 
-
-  const [getData, setgetData]=useState([])
+  const [getDataFromgitHubUrl, setgetData]=useState([])
 
   useEffect(() =>{
     axios.get(gitHubUrl)
     .then(response =>{setgetData(response.data) })
+  })
+
+  const [getDataFromgitHubUrl2 , setgetDataFromgitHubUrl2]=useState([])
+
+  useEffect(() =>{
+    axios.get(gitHubUrl2)
+    .then(response =>{setgetDataFromgitHubUrl2(response.data) })
   })
   
 
@@ -41,7 +42,31 @@ function Tables() {
         </TableRow>
       </TableHead>
       <TableBody>
-        {getData.map((row)=> (<TableRow key={row.id}>
+        {getDataFromgitHubUrl.map((row)=> (<TableRow key={row.id}>
+          
+          <TableCell>{row.name}</TableCell>
+          <TableCell>{row.language || "-"}</TableCell>
+          <TableCell>{row.description || "-"}</TableCell>
+        </TableRow>))}
+      </TableBody>
+    </Table>
+
+  </TableContainer>
+
+  <TableContainer component={Paper}>
+     
+    <Table className='table'>
+      <TableHead>
+        <TableRow>
+          
+          <TableCell>Project Name</TableCell>
+          <TableCell>Language</TableCell>
+          <TableCell>Description</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        
+        {getDataFromgitHubUrl2.map((row)=> (<TableRow key={row.id}>
           
           <TableCell>{row.name}</TableCell>
           <TableCell>{row.language || "-"}</TableCell>
